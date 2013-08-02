9cf660f6a13090daeb63dd5c0969e342781c7c16
Kevin Wu <kwu@salesforce.com>
2013-08-02 15:34:53 -0700

    moved around usemin modules. changed back to local storage.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,14 +1,14 @@
 'use strict';
 
 angular.module('todoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.todos = ['item 1', 'item 2', 'item 3'];
+  .controller('MainCtrl', function ($scope, localStorageService) {
+    //$scope.todos = ['item 1', 'item 2', 'item 3'];
 
-    // var todosInStore = localStorageService.get('todos');
-    // $scope.todos = todosInStore && todosInStore.split('\n') || [];
-    // $scope.$watch(function() {
-    //   localStorageService.add('todos', $scope.todos.join('\n'));
-    // });
+    var todosInStore = localStorageService.get('todos');
+    $scope.todos = todosInStore && todosInStore.split('\n') || [];
+    $scope.$watch(function() {
+      localStorageService.add('todos', $scope.todos.join('\n'));
+    });
 
     $scope.addTodo = function() {
       $scope.todos.push($scope.todo);
